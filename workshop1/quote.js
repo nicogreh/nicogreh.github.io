0c79d6b2a18cc6b163504d68387c81904405b877
nicogreh <nicogreh@gmail.com>
2016-08-17 10:09:26 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,8 +4,8 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var things = ["Tomfoolery", "Horseplay", "Nonsense", "Bruhaha", "Shenanigans", "Hooliganary"];
+var programmers = ["fools", "fiends", "jokers", "flunkys", "spunkys", "trunkys"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
